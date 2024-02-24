b89cfed70a1de3e32d2a96dbd6016435a6de4e0c
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2024-02-24 18:10:24 +0800

    Create log.h

---
+++ src/log.h
@@ -0,0 +1,40 @@
+/*
+    Author: (Bonezegei) Jofel Batutay
+    Date: February 2024
+    Purpose: Simple Log Class
+*/
+
+#ifndef __LOG_H_INCLUDED__
+#define __LOG_H_INCLUDED__
+
+#include <stdio.h>
+#define MAX_LOG         128
+#define MAX_LOG_STRING  128
+
+enum LogType{ Unknown=0, Warning, Error, Success };
+
+typedef struct {
+    unsigned int timestamp;
+    LogType      logType;
+    char         logTypeStr[32];
+    char         msg[MAX_LOG_STRING];
+}LOG;
+
+class Log{
+public:
+    Log();
+
+    void add(int index, LogType type, const char *message);
+    void add(LogType type, const char *message);
+
+    int  getNumberOfLogs();
+    LOG  getLog(int index);
+
+private:
+    const char *LogTypeStr[4]={"Unknown", "Warning", "Error", "Success"};
+
+    int numberOfLogs;
+    LOG log[MAX_LOG];
+};
+
+#endif
